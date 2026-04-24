fbac78cbd67c98c6248b3228857d34843310b691
dhananjay <dhananjay1.more@gmail.com>
2026-04-24 18:34:35 +0530

    first commit

---
+++ src/test/java/Rahul_Shetty_Practice_Projects/Assignment/Assignment1.java
@@ -0,0 +1,38 @@
+package Rahul_Shetty_Practice_Projects.Assignment;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class Assignment1
+{
+
+	public static void main(String[] args)
+	{
+		System.setProperty("Webdriver.chrome.driver",
+				"C://Users//Admin//Downloads//chromedriver-win64_ver117.01//chromedriver-win64//chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+
+		// Working with checkboxes
+		driver.findElement(By.xpath("//input[@name='checkBoxOption1']")).click();
+		Assert.assertTrue(driver.findElement(By.xpath("//input[@name='checkBoxOption1']")).isSelected());
+		driver.findElement(By.xpath("//input[@name='checkBoxOption1']")).click();
+		Assert.assertFalse(driver.findElement(By.xpath("//input[@name='checkBoxOption1']")).isSelected());
+
+		// To get the total number of checkboxes
+		System.out.println(driver.findElement(By.xpath("//input[@type='checkbox']")).getSize());
+		driver.quit();
+		
+		System.out.println("Test1 for Git clone");
+		System.out.println("Test2 for Git clone");
+		System.out.println("Test3 for Git clone");
+		
+		System.out.println("updates1 in develop branch");
+		System.out.println("updates2 in develop branch");
+
+	}
+
+}
